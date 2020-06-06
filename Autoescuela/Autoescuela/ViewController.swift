b5abeb26ccf5ac0a3240cde2d4d4cac30b331e05
Apostol123 <apostol516@gmail.com>
2020-06-06 15:33:39 +0100

    firstCommit

---
+++ Autoescuela/Autoescuela/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autoescuela
+//
+//  Created by user159106 on 6/6/20.
+//  Copyright © 2020 user159106. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
